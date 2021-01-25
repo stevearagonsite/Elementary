b1b042e94d6b8cdf8eded6af80ded83de9faccf8
Cristian Vigo <cristian.vigo@davinci.edu.ar>
2021-01-24 22:04:09 -0300

    #174 | cinematica cuando inicia el nivel

---
+++ Assets/MyContent/Scripts/ReWorkedScripts/NewScripts/StoryTelling/StartCinematicStory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMoveOnCinematic))]
+public class StartCinematicStory : MonoBehaviour
+{
+    private PlayerMoveOnCinematic _move;
+
+    private void Start()
+    {
+        _move = GetComponent<PlayerMoveOnCinematic>();
+        EventManager.AddEventListener(GameEvent.TRANSITION_FADEIN_FINISH, OnStartScreen);
+    }
+
+    private void OnStartScreen(object[] pC)
+    {
+        EventManager.RemoveEventListener(GameEvent.TRANSITION_FADEIN_FINISH, OnStartScreen);
+        StartCoroutine(StartCinematic());
+    }
+
+    private IEnumerator StartCinematic()
+    {
+        yield return new WaitForEndOfFrame();
+        EventManager.DispatchEvent(GameEvent.STORY_START, _move.nodes);
+        Destroy(this);
+    }
+}
