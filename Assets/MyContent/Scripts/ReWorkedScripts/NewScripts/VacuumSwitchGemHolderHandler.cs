b707b2e8e254243748bedf762606f35f858c167e
Cristian Vigo <cristian.vigo@davinci.edu.ar>
2021-02-21 20:28:50 -0300

    #163 | hacerle el timing del progreso al puzzle de aguila de forma visual

---
+++ Assets/MyContent/Scripts/ReWorkedScripts/NewScripts/VacuumSwitchGemHolderHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumSwitchGemHolderHandler : MonoBehaviour
+{
+    private VacuumSwitch _switch;
+    public float rotationSpeed;
+    private float actualSpeed;
+    void Start()
+    {
+        _switch = GetComponentInParent<VacuumSwitch>();
+        UpdatesManager.instance.AddUpdate(UpdateType.UPDATE, Rotate);
+    }
+
+    public void OnChangeCharge()
+    {
+        actualSpeed = _switch.currentAmountOfAir / _switch.maxAmountOfAir * rotationSpeed * Time.deltaTime;
+    }
+    public void Rotate()
+    {
+        transform.Rotate(0,0,actualSpeed);
+    }
+    public void OnActivation()
+    {
+        actualSpeed = rotationSpeed;
+    }
+
+    private void OnDestroy()
+    {
+        UpdatesManager.instance.RemoveUpdate(UpdateType.UPDATE, Rotate);
+    }
+}
