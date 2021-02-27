21b806c4158396422875c69d7d87563173b9aa73
Cristian Vigo <cristian.vigo@davinci.edu.ar>
2021-02-27 15:59:37 -0300

    #179 | Crear botones de testing

---
+++ Assets/MyContent/Scripts/ReWorkedScripts/NewScripts/Testing/CheatLevelSelect.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheatLevelSelect : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-01", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-02", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-03", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-04", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-05", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.F6))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-06", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.F7))
+        {
+            UnloadCurrentLevel();
+            SceneLoadManager.instance.LoadSceneAsync("Level-07", LoadSceneMode.Additive);
+        }
+    }
+
+    private void UnloadCurrentLevel()
+    {
+        
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            var scene = SceneManager.GetSceneAt(i);
+            if (scene.name.Contains("Level"))
+            {
+                SceneLoadManager.instance.UnloadSceneAsync(scene.name);
+            }
+        }
+    }
+}
