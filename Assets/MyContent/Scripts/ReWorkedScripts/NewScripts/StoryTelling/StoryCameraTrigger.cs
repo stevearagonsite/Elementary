44fb32f7803247c6610d07acacb4799335a4be70
Cristian Vigo <cristian.vigo@davinci.edu.ar>
2021-03-03 19:58:18 -0300

    #330 | cámara animada activable por callback

--- Assets/MyContent/Scripts/ReWorkedScripts/NewScripts/StoryTelling/StoryCameraTrigger.cs
+++ Assets/MyContent/Scripts/ReWorkedScripts/NewScripts/StoryTelling/StoryCameraTrigger.cs
@@ -7,7 +7,14 @@ public class StoryCameraTrigger : MonoBehaviour
     public Camera storyCam;
     private void OnTriggerEnter(Collider other)
     {
+        ActivateCameraStory();
+    }
+
+    public void ActivateCameraStory()
+    {
         EventManager.DispatchEvent(GameEvent.CAMERA_STORY, storyCam);
         Destroy(this);
     }
 }
+
+
